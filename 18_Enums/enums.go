b8402149222c058d9dcd346889d8b0ce45739573
rohitjoijodedev07 <rohit.joijode.dev07@gmail.com>
2026-09-19 23:32:55 -0700

    commited changes on 19-09-26

---
+++ 18_Enums/enums.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// enumerated types
+
+type OrderStatus string
+
+// type OrderStatus int
+
+// const (
+// 	Received  OrderStatus = iota
+// 	Confirmed
+// 	Prepared
+// 	Delivered
+// )
+
+const (
+	Received  OrderStatus = "received"
+	Confirmed             = "confirmed"
+	Prepared              = "prepared"
+	Delivered             = "delivered"
+)
+
+func changeOrderStatus(status OrderStatus) {
+	fmt.Println("changing order status to", status)
+}
+
+func main() {
+	changeOrderStatus(Prepared)
+}
